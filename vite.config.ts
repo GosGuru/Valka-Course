ce112b1242f375c063dc2f60826ca0acab79ffe2
GosGuru <mporcile59@gmail.com>
2025-08-27 16:49:10 -0300

    oa

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/Valka-Course/', // Comentado temporalmente para pruebas
+  base: '/Valka-Course/', // Nombre de tu repositorio
 })
